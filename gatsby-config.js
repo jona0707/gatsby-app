23f615858b18abbb17f50a3381f3c32cde96f845
Jona <jona07072000@gmail.com>
2024-02-18 20:33:00 -0500

    adding About page to site

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,9 +9,9 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `App Web con Gatsby`,
+    description: `Creación de una aplicación web utilizando Gatsby. Esto se recuperará a través de una consulta.`,
+    author: `@gastbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
